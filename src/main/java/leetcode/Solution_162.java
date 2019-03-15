2220ed8febf99173436f98cccb644b8d30459d95
Xuantang Cun <cunxuantang@meituan.com>
2019-03-15 20:02:41 +0800

    add

--- src/main/java/leetcode/Solution_162.java
+++ src/main/java/leetcode/Solution_162.java
@@ -7,21 +7,16 @@ public class Solution_162 {
 
     @Test
     public void Solution_162_Test() {
-        Assert.assertEquals(2, findPeakElement(new int[]{1, 2, 1, 2, 4, 5}));
+        Assert.assertEquals(6, findPeakElement(new int[]{1, 2, 1, 2, 4, 5, 6}));
     }
 
     public int findPeakElement(int[] nums) {
-        int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            int mid = (r - l) / 2 + l;
-            if ((mid <= l || nums[mid] > nums[mid - 1]) &&
-                    (mid >= r || nums[mid] > nums[mid + 1]))
-                return mid;
-            if (mid <= l || nums[mid] > nums[mid - 1])
-                l = mid + 1;
-            else
-                r = mid - 1;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
         }
-        return -1;
+        return left;
     }
 }
